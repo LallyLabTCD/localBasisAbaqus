d280af66df08a363d2e4c363712ffaabd77395c1
David Nolan <DNOLAN4@tcd.ie>
2019-02-04 18:28:49 +0000

    Renamed some files and uploaded new files for new sections
    These new Abaqus files will need to be reorganised.

--- Lesson A - deformation gradient/Matlab/Worked_example_1.m
+++ Lesson A - deformation gradient/Matlab/Worked_example_1.m
@@ -1,82 +1,83 @@
-clear variables; clc
-% Script to demonstrate the issues around material objectivity described in
-% the manuscript
-%
-% David Nolan
-%
-%% Kinematics
+clear variables
 
-% Deformation gradient in global basis, FG
-% N.B. This is what Abaqus passes to the UMAT when no local coordinate
-% system is implemented.
-FG = [1.10   0.10   0.00
-      0.05   0.90   0.15
-      0.20   0.00   1.20];      % Eq. (4)
+%% Continuum element where E_i = G_i
+% Basic deformation
+F = [1 0 0
+     0 1.1 0
+     0 0 1/1.1];
 
-% Compute a polar decomposition of the deformation gradient
-[UG, RG]=polardecomp(FG);
+% Rotate by 45 degrees
+t=45;
+T = [cosd(t)   sind(t) 0
+     -sind(t)  cosd(t) 0
+       0         0    1];
 
-% Calculate the local deformation gradient
-% This is the same transform that Abaqus uses when you use *orientation
-FL = RG'*FG*RG;                 % Eq. (3) & (6)
-[UL, RL]=polardecomp(FL);       % Eq. (1)
+% Rotate by 45 degrees. DG in the global basis
+F = T*F;
+disp(F)
 
-% Note the the eigenvalues of the right stretch tensor U are the same. We
-% expect to see this invariance.
-[lambdaGvec, lambdaGval] = eigs(UG);
-[lambdaLvec, lambdaLval] = eigs(UL);
+% Determine rotational part of the deformation.
+[U,R]=polardecomp(F);
 
-%% Basis vectors
-% Global basis vectors e0G
-G1 = [1 0 0]';
-G2 = [0 1 0]';
-G3 = [0 0 1]';
+% "Abaqus Local" deformation gradient
+Fal = R'*F*R;
 
-% The local basis vectors in the reference configuration. Note that in this 
-% case G_i = E_i
-E1 = G1;
-E2 = G2;
-E3 = G3;
+%% Shell element where E_i = G_i
 
-% Calculate the local basis vectors in the current configuration.
-eL1 = RG*E1;
-eL2 = RG*E2;
-eL3 = RG*E3;
+% Polar decomposition to determine R  
+[U,R]=polardecomp(F);
 
-
-%% Plotting
-figure(1);clf;
-title('Local Basis System')
-% Plot the deformed unit cube
-dfgrdplot(FG,'w')
-
-% Plot and label the global basis vectors
-quiver3(0,0,0,G1(1),G1(2),G1(3),'k','LineWidth',2,'MaxHeadSize',0.4)
-quiver3(0,0,0,G2(1),G2(2),G2(3),'k','LineWidth',2,'MaxHeadSize',0.4)
-quiver3(0,0,0,G3(1),G3(2),G3(3),'k','LineWidth',2,'MaxHeadSize',0.4)
-
-G = [G1, G2 G3];
-strings = {'$$\textbf{G}_{1}$$';'$$\textbf{G}_{2}$$';'$$\textbf{G}_{3}$$'}; 
-text(G(1,:),G(2,:),G(3,:),strings,'Interpreter','latex','FontSize',14)
-
-% Plot and label the local basis vectors in the current configuration
-quiver3(0.5,0.5,0.5,eL1(1),eL1(2),eL1(3),'r','LineWidth',2,'MaxHeadSize',0.4)
-quiver3(0.5,0.5,0.5,eL2(1),eL2(2),eL2(3),'g','LineWidth',2,'MaxHeadSize',0.4)
-quiver3(0.5,0.5,0.5,eL3(1),eL3(2),eL3(3),'b','LineWidth',2,'MaxHeadSize',0.4)
-
-e0L = [eL1, eL2 eL3] + 0.5;
-strings = {'$$\textbf{e}_{1}$$';'$$\textbf{e}_{2}$$';'$$\textbf{e}_{3}$$'}; 
-text(e0L(1,:),e0L(2,:),e0L(3,:),strings,'Interpreter','latex','FontSize',14)
-
-% Set axis limits
-xlim([-0.2 1.5])
-ylim([-0.2 1.5])
-zlim([-0.2 1.5])
+% "Classical" local deformation gradient
+Fd = R'*F;
  
-% Label the basis vectors
-e0L = [eL1, eL2 eL3] + 0.5;
-strings = {'$$\textbf{e}_{1}$$';'$$\textbf{e}_{2}$$';'$$\textbf{e}_{3}$$'}; 
-text(e0L(1,:),e0L(2,:),e0L(3,:),strings,'Interpreter','latex','FontSize',14)
+%% Continuum element where E_i \= G_i
+% Rotation matrix to rotate basis in reference configuration by 20 degrees
+t=20;
+Q = [cosd(t) -sind(t) 0
+     sind(t)  cosd(t) 0
+       0         0    1];
 
-% Set the camera view
-view(125,25)
+% "Abaqus Local" deformation gradient where the reference basis is not
+% aligned with the global basis
+Fal20 = Q'*Fal*Q;
+
+%% Shell element where E_i \= G_i
+% Rotation matrix to rotate basis
+t=20;
+Q = [cosd(t) -sind(t) 0
+     sind(t)  cosd(t) 0
+       0         0    1];
+
+% "Classical" local deformation gradient where the reference basis is not
+% aligned with the global basis
+Fd20 = Q'*Fd*Q;
+
+%% Display Results
+clc
+disp('***** Continuum Element: Global Basis *****')
+disp(' ')
+disp('F = ')
+disp(F)
+disp(' ')
+disp('***** Continuum Element: Abaqus Local Basis *****')
+disp(' ')
+disp('Fal = ')
+disp(Fal)
+disp(' ')
+disp('***** Structural Element *****')
+disp(' ')
+disp('Fd = ')
+disp(Fd)
+disp(' ')
+disp('***** Continuum Element: E_i neq G_i *****')
+disp(' ')
+disp('Fal20 = ')
+disp(Fal20)
+disp(' ')
+disp('***** Structural Element: E_i neq G_i *****')
+disp(' ')
+disp('Fd20 = ')
+disp(Fd20)
+disp(' ')
+   
+   
--- Lesson A - deformation gradient/Matlab/Worked_example_3.m
+++ Lesson A - deformation gradient/Matlab/Worked_example_3.m
@@ -4,27 +4,36 @@ clear variables; clc
 %
 % David Nolan
 %
-%% Kinematics
+%%
 
 % Deformation gradient in global basis, FG
 % N.B. This is what Abaqus passes to the UMAT when no local coordinate
 % system is implemented.
 FG = [1.10   0.10   0.00
       0.05   0.90   0.15
-      0.20   0.00   1.20];      % Eq. (4)
+      0.20   0.00   1.20];      % Eq. (4);
 
-% Compute a polar decomposition of the deformation gradient
-[UG, RG]=polardecomp(FG);
+% Rotation matrix used to change the basis in the reference
+% configuration.
+t=pi/9; % 20 degrees in radians
+Q = [cos(t) -sin(t) 0
+     sin(t)  cos(t) 0
+       0     0      1];         % Eq. (7)
 
-% Calculate the local deformation gradient
-% This is the same transform that Abaqus uses when you use *orientation
-FL = RG'*FG*RG;                 % Eq. (3) & (6)
-[UL, RL]=polardecomp(FL);       % Eq. (1)
+% Change the basis of the global deformation gradient
+FG20 = Q'*FG*Q;                 % Eq. (8)
+
+% Polar decomposition of the global deformation gradient in the new basis
+[UG20, RG20]=polardecomp(FG20);
+
+% Map the global deformation gradient to the local deformation gradient
+FL20 = RG20'*FG20*RG20;         % Eq. (9)
+[UL20, RL20]=polardecomp(FL20);
 
 % Note the the eigenvalues of the right stretch tensor U are the same. We
 % expect to see this invariance.
-[lambdaGvec, lambdaGval] = eigs(UG);
-[lambdaLvec, lambdaLval] = eigs(UL);
+[lambdaGvec, lambdaGval] = eigs(UG20);
+[lambdaLvec, lambdaLval] = eigs(UL20);
 
 %% Basis vectors
 % Global basis vectors e0G
@@ -32,21 +41,20 @@ G1 = [1 0 0]';
 G2 = [0 1 0]';
 G3 = [0 0 1]';
 
-% The local basis vectors in the reference configuration. Note that in this 
-% case G_i = E_i
-E1 = G1;
-E2 = G2;
-E3 = G3;
+% The basis vectors in the reference configuration.
+E1 = Q*G1;
+E2 = Q*G2;
+E3 = Q*G3;
 
-% Calculate the local basis vectors in the current configuration.
-eL1 = RG*E1;
-eL2 = RG*E2;
-eL3 = RG*E3;
+% Calculate the basis vectors in the current configuration.
+eL1 = RG20*E1;
+eL2 = RG20*E2;
+eL3 = RG20*E3;
 
 
 %% Plotting
-figure(1);clf;
-title('Local Basis System')
+figure(2);clf;
+title('Local Basis System when E_i \neq G_i')
 % Plot the deformed unit cube
 dfgrdplot(FG,'w')
 
@@ -59,7 +67,16 @@ G = [G1, G2 G3];
 strings = {'$$\textbf{G}_{1}$$';'$$\textbf{G}_{2}$$';'$$\textbf{G}_{3}$$'}; 
 text(G(1,:),G(2,:),G(3,:),strings,'Interpreter','latex','FontSize',14)
 
-% Plot and label the local basis vectors in the current configuration
+% Plot and label the local basis vectors in the undefromed config.
+quiver3(0,0,0,E1(1),E1(2),E1(3),'Color',[0.87 0.49 0],'LineWidth',2,'MaxHeadSize',0.4)
+quiver3(0,0,0,E2(1),E2(2),E2(3),'Color',[0.87 0.49 0],'LineWidth',2,'MaxHeadSize',0.4)
+quiver3(0,0,0,E3(1),E3(2),E3(3),'Color',[0.87 0.49 0],'LineWidth',2,'MaxHeadSize',0.4)
+
+E = [E1, E2 E3];
+strings = {'$$\textbf{E}_{1}$$';'$$\textbf{E}_{2}$$';'$$\textbf{E}_{3}$$'}; 
+text(E(1,:),E(2,:),E(3,:),strings,'Interpreter','latex','FontSize',14)
+
+% Plot and label the local basis vectors
 quiver3(0.5,0.5,0.5,eL1(1),eL1(2),eL1(3),'r','LineWidth',2,'MaxHeadSize',0.4)
 quiver3(0.5,0.5,0.5,eL2(1),eL2(2),eL2(3),'g','LineWidth',2,'MaxHeadSize',0.4)
 quiver3(0.5,0.5,0.5,eL3(1),eL3(2),eL3(3),'b','LineWidth',2,'MaxHeadSize',0.4)
@@ -69,9 +86,9 @@ strings = {'$$\textbf{e}_{1}$$';'$$\textbf{e}_{2}$$';'$$\textbf{e}_{3}$$'};
 text(e0L(1,:),e0L(2,:),e0L(3,:),strings,'Interpreter','latex','FontSize',14)
 
 % Set axis limits
-xlim([-0.2 1.5])
-ylim([-0.2 1.5])
-zlim([-0.2 1.5])
+xlim([-0.4 1.5])
+ylim([-0.4 1.5])
+zlim([-0.4 1.5])
  
 % Label the basis vectors
 e0L = [eL1, eL2 eL3] + 0.5;
